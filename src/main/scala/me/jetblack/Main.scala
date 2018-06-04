c2a0436821c2ed48fa22bad2e12abae7f707c20e
Ovechkin Dmitry <ovechkin.dm@gmail.com>
2018-06-04 11:50:47 +0300

    add example

--- src/main/scala/me/jetblack/Main.scala
+++ src/main/scala/me/jetblack/Main.scala
@@ -5,6 +5,8 @@ import me.jetblack.model.first.first.FirstMessage
 object Main extends App {
 
   val firstMessage = FirstMessage(10)
+  val javaFirstMessage = FirstMessage.toJavaProto(firstMessage)
+  println(javaFirstMessage)
   println(firstMessage)
 
 }
